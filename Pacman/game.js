afb929902312cb85186c26961a646149a615bae0
Koi <Alexa24235@live.ytech.edu>
2024-03-08 22:36:46 -0500

    Finished Pacman's Enemy Collision Detection

--- Pacman/game.js
+++ Pacman/game.js
@@ -99,6 +99,9 @@ let update = () => {
     for (let i = 0; i < ghosts.length; i++) {
         ghosts[i].moveProcess();
     };
+    if (pacman.checkGhostCollision()) {
+        //console.log("Hit");
+    };
 };
 
 let draw = () => {
