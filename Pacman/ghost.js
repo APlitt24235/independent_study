628d2fe6175df59f6c2b041d1322608f57d6a242
Koi <Alexa24235@live.ytech.edu>
2024-03-08 20:52:22 -0500

    Finished Ghosts' Directional Movement
    (As well as the following: Forwards Movement, Collision Detection, Position Retrieval, Backwards Movement, and Movement Process)

--- Pacman/ghost.js
+++ Pacman/ghost.js
@@ -18,4 +18,81 @@ class Ghost {
         canvasContext.drawImage(ghostFrames, this.imageX, this.imageY, this.imageWidth, this.imageHeight, this.x, this.y, this.width, this.height);
         canvasContext.restore();
     };
+
+    changeDirectionIfPossible() {
+        let tempDirection = this.direction;
+        this.direction = this.calculateNewDirection(map, parseInt(this.target.x / oneBlockSize), parseInt(this.target.y / oneBlockSize));
+        this.moveForwards();
+        if (this.checkCollision()) {
+            this.moveBackwards();
+            this.direction = tempDirection;
+        } else {
+            this.moveBackwards();
+        };
+    };
+
+    moveForwards() {
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y -= this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_BOTTOM:
+                this.y += this.speed;
+                break;
+        };
+    };
+
+    checkCollision() {
+        if (map[this.getMapY()][this.getMapX()] == 1 || map[this.getMapYRightSide()][this.getMapX()] == 1 || map[this.getMapY()][this.getMapXRightSide()] == 1 || map[this.getMapYRightSide()][this.getMapXRightSide()] == 1){
+            return true;
+        };
+        return false;
+    };
+
+    getMapY() {
+        return parseInt(this.y / oneBlockSize);
+    };
+
+    getMapX() {
+        return parseInt(this.x / oneBlockSize);
+    };
+
+    getMapYRightSide() {
+        return parseInt((this.y + 0.9999 * oneBlockSize) / oneBlockSize);
+    };
+
+    getMapXRightSide() {
+        return parseInt((this.x + 0.9999 * oneBlockSize) / oneBlockSize);
+    };
+
+    moveBackwards() {
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y += this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_BOTTOM:
+                this.y -= this.speed;
+                break;
+        };
+    };
+
+    moveProcess() {
+        this.changeDirectionIfPossible();
+        this.moveForwards();
+        if (this.checkCollision()) {
+            this.moveBackwards();
+        };
+    };
 };
