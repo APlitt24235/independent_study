9d3138ba1ecc032ba9ae153c7da8bb6d0a8eb4d7
Koi <Alexa24235@live.ytech.edu>
2024-03-08 20:33:46 -0500

    Created New Ghost Instances

--- Pacman/game.js
+++ Pacman/game.js
@@ -15,6 +15,7 @@ let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
 let foodColor = "#FEB897";
 let score = 0;
+let ghosts = [];
 
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -40,6 +41,13 @@ let map = [
     [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+let ghostLocations = [
+    {x: 0, y: 0},
+    {x: 176, y: 0},
+    {x: 0, y: 121},
+    {x: 176, y: 121}
 ];
 
 let createRect = (x, y, width, height, color) => {
@@ -110,6 +118,13 @@ let drawScore = () => {
     canvasContext.fillText("Score: " + score, 0, oneBlockSize * (map.length + 1) + 10);
 };
 
+let createGhosts = () => {
+    ghosts = [];
+    for (let i = 0; i < 1; i++) {
+        let newGhost = new Ghost(9 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize, 10 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize, oneBlockSize, oneBlockSize, pacman.speed / 2, ghostLocations[i % 4].x, ghostLocations[i % 4].y, 124, 116, 6 + i);
+    };
+};
+
 window.addEventListener("keydown", (event) => {
     let k = event.keyCode;
     setTimeout(() => {
@@ -132,4 +147,5 @@ window.addEventListener("keydown", (event) => {
 let gameInterval = setInterval(gameLoop, 1000 / fps);
 
 createNewPacman();
+createGhosts();
 gameLoop();
